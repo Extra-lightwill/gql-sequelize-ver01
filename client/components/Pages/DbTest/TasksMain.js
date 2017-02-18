a50d2fb7fc0b962e5ae4042df62c49bee16886a4
robrobkay51 <singingbottle@gmail.com>
2017-02-18 23:41:59 +0000

    commmitchanges rename

--- client/components/Pages/DbTest/TasksMain.js
+++ client/components/Pages/DbTest/TasksMain.js
@@ -21,13 +21,15 @@ class TasksMain extends React.Component {
 
     this._onNewTaskSave = this._onNewTaskSave.bind(this);
     this._handleTextInputCancel = this._handleTextInputCancel.bind(this);
+
+    this._commitChanges = this._commitChanges.bind(this);
   }
 
  
-    _onNewTaskSave = (text) => {
+    _onNewTaskSave = (stext) => {
 
     this.props.relay.commitUpdate(
-      new addTaskMutation({ viewer: this.props.viewer, text })
+      new addTaskMutation({ viewer: this.props.viewer, stext })
     );
   };
 
@@ -65,7 +67,7 @@ class TasksMain extends React.Component {
     if (this._handleTextInputCancel && e.keyCode === ESC_KEY_CODE) {
       this._handleTextInputCancel();
     } else if (e.keyCode === ENTER_KEY_CODE) {
-      this._onNewTaskSave();
+      this._commitChanges();
     }
   };
  
@@ -80,16 +82,28 @@ class TasksMain extends React.Component {
   };
 
 
-   commitUpdate() {
+   _commitChanges() {
     const newText = this.state.text.trim();
-    if (newText) {
+    if (newText !== '') {
       this.setState({ text: '' });
     }
   }
 
+  /*else if (newText !== '') {
+      this.props.onSave(newText);
+      this.setState({text: ''}) */
+
+    /*_handleTextInputSave = (text) => {
+    this._setEditMode(false);
+    this.props.relay.commitUpdate(
+      new RenameTodoMutation({tasks: this.props.todo, text})
+    );*/
+
     _handleTextInputCancel = () => {
     this._setEditMode(false);
   };
+
+
   
 
 	render () {	
